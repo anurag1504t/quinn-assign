64c5882ff483ef459620e6eefabdd3c31198332e
Anurag Tiwari <anurag1504t@gmail.com>
2021-04-13 15:56:39 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import { Card } from "reactstrap";
 
 function App() {
 	const [arr, setArr] = useState([]);
-	const [minDate, setMinDate] = useState(new Date());
-	const [maxDate, setMaxDate] = useState("");
+	const [minDate, setMinDate] = useState();
+	const [maxDate, setMaxDate] = useState();
+	const monthDays = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
 	useEffect( () => {
 		let today = new Date();
 		let day = today.getDay();
+		console.log(today);
 		let tempArr = [];
 		let tempToday = today;
 		for(var i = 0; i < 90 +120 +120 + day - 7;i++) {
@@ -24,41 +27,70 @@ function App() {
 		tempArr = tempArr2.concat(tempArr);
 		tempArr[0] = ( d => new Date(d.setDate(d.getDate()-1)) )(tempArr[0]);
 		setArr(tempArr);
+		// console.log(minDate);
+		// console.log(maxDate);
 
 	}, []);
 
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll);
 		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
+	}, [maxDate]);
 	
 	function handleScroll() {
+		console.log(document.documentElement.scrollTop);
 		if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
 		console.log('Fetch more list items!');
 		let tempArr = [];
-		let today = maxDate;
-		console.log(today);
+		let today = new Date(maxDate);
+		// console.log(today);
 		for(var i = 0; i < 91;i++) {
-			today = ( d => new Date(d.setDate(d.getDate()+1)) )(today);
 			tempArr.push(today);
+			today = ( d => new Date(d.setDate(d.getDate()+1)) )(today);			
 		}
 		tempArr = [...arr, ...tempArr];
 		setArr(tempArr);
-		console.log(arr);
+		// console.log(arr);
+		setMaxDate(today);
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScrollTop);
+		return () => window.removeEventListener('scroll', handleScrollTop);
+	}, [minDate]);
+	
+	function handleScrollTop() {
+		if (window.innerHeight !== document.documentElement.offsetHeight) return;
+		console.log('Top');
 	}
 
 	return (
 		<div className="container">
+			<div className="top-bar fixed calender-header">
+				<div className="header">
+					<span className="month-header">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long'}).format(new Date())} </span>
+				</div>
+				<div className="day-tile">
+					{
+						monthDays?
+							monthDays.map( (day, index) => {
+								return (
+									<div className="day-tile">
+										<div className="day"><strong>{day}</strong> </div>
+									</div>
+								)
+							})
+							:<div></div>
+					}
+				</div>
+			</div>
 			<div className="tile">
 				{	
 					arr?
 						arr.map((date, index)=>{
 							return(
 								<div className='calender-tile' key={index}>
-									<div className='date-tile'>
-										<div>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(date)))}</div>
-									</div>
-							
+									<Tile date={date} />							
 								</div>
 							)
 						}):<div></div>
@@ -66,6 +98,16 @@ function App() {
 			</div>
 		</div>
 	);
+}
+
+const Tile = ({date}) => {
+	return (
+		<Card>
+			<div className='date-tile'>
+				<div>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(date)))}</div>
+			</div>
+		</Card>
+	)
 }
 
 export default App;
